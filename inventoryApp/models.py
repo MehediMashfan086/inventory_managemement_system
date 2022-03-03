5346b704c2007566c3b12c849b0998e70ddd763e
MehediMashfan086 <mehedihasan62086@gmail.com>
2022-03-03 17:14:58 +0600

    2nd commit

--- inventoryApp/models.py
+++ inventoryApp/models.py
@@ -1,3 +1,31 @@
+from random import choice
 from django.db import models
 
 # Create your models here.
+class Device(models.Model): #name of the table
+    type = models.CharField(max_length=100, blank=False) # name of the colmn
+    price = models.IntegerField()
+
+    choices = (
+        ('AVAILABLE', 'Item ready to purchased'),
+        ('SOLD', 'Item sold'),
+        ('RESTOCKING', 'Item restocking in few days')
+    )
+
+    status = models.CharField(max_length=10, choices=choices, default="SOLD") #Available, Sold, Restocking
+    issues = models.CharField(max_length=100, default="No issues")
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return 'Type: {0} Price: {1}'.format(self.type, self.price)
+
+class Laptop(Device):
+    pass
+
+class Desktop(Device):
+    pass
+
+class Mobile(Device):
+    pass
